9edc71ac9e96b4692351e3ce9fe79755b8678af7
sayalimartal <41475386+sayalimartal@users.noreply.github.com>
2018-08-04 17:23:05 +0530

    Add files via upload

---
+++ src/SumOfThreeNumbers.java
@@ -0,0 +1,10 @@
+//Display sum of three numbers passed using command line arguments 
+public class SumOfThreeNumbers {
+
+	public static void main(String[] args) {
+
+		//Display sum
+		System.out.println("Sum of three numbers is "+(Integer.parseInt(args[0])+Integer.parseInt(args[1])+Integer.parseInt(args[2])));
+		
+	}
+}
